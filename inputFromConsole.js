9c385716d093c45f5d9f48ee02d1200e05cd6c6f
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-06 08:39:46 +0530

    changes in inputFromConsole

--- inputFromConsole.js
+++ inputFromConsole.js
@@ -1,7 +1,8 @@
-console.log("Input from console.");
+const colors = require("colors");
+console.log("Input from console.".green);
 
 console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-console.log("First Name: "+process.argv[2]);
-console.log("Last Name: "+process.argv[3]);
-console.log("Age: "+process.argv[4]);
+console.log("First Name: ".green+process.argv[2]);
+console.log("Last Name: ".green+process.argv[3]);
+console.log("Age: ".green+process.argv[4]);
 console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
