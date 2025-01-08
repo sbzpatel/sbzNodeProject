2bae2b31b95a0d640723bc06a1e2ef04e02dc885
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-08 19:16:06 +0530

    added file details in index file

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 console.log("Its index.js, entry level file.");
 
-console.log("1) basicConcept.js");
-console.log("2) coreModule.js");
+console.log("################################# NodeJS ###############################");
+    console.log("1) basicConcept.js");
+    console.log("2) coreModule.js");
+    console.log("3) thirdPartyPackage.js");
+    console.log("4) myServer.js");
+    console.log("5) simpleApi.js");
+    console.log("6) inputFromConsole.js");
+    console.log("7) readDirFilenames.js");
+    console.log("8) fileOperations.js");
+    console.log("9) asyncBasics.js");
+    console.log("10) myPromiseThen.js");
+    console.log("11) myExpressPackage.js");
+    console.log("12) routeParams.js");
+    console.log("13) renderHtmlnJson.js");
+    console.log("14) renderHtmlPage.js");
+    console.log("15) removeExtension.js");
+    console.log("16) handle404Page.js");
+    console.log("17) expressMiddleware.js");
+    console.log("18) middleware.js");
+    console.log("19) routeLevelMiddleware.js");
+console.log("################################# NodeJS ###############################");
+
+console.log("################################# MongoDB ###############################");
+    console.log("20) myMongoose.js");
+    console.log("21) mongooseSchemaModel.js");
+    console.log("22) insertDocument.js");
+    console.log("23) multipleInsertDocument.js");
+    console.log("24) getDocument.js");
+    console.log("25) comparisonOperators.js");
+    console.log("26) logicalOperators.js");
+    console.log("27) sortCountMethods.js");
+    console.log("28) updateDocument.js");
+    console.log("29) deleteDocument.js");
+console.log("################################# MongoDB ###############################");
