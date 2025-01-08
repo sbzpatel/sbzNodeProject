47871e760c5cf5dca252dac6e4101db41d222e9a
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-08 18:12:05 +0530

    minor changes done

--- coreModule.js
+++ coreModule.js
@@ -1,3 +1,5 @@
-const fs = require("fs");
+const fs = require("fs");   // fs is a non-global module, need to import to use it
 
-fs.writeFileSync("abc.txt", "Its a file created by fileSystem here....")
+console.log("Shahbaz Patel, Aurwad");   // "console" is a core module to display message on console
+
+fs.writeFileSync("abc.txt", "Its a file created by fileSystem here....");  
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log("Its index.js, entry level file.");
+
+console.log("1) basicConcept.js");
+console.log("2) coreModule.js");
