66c6bc6feef902467714decd4471d8dc87155ca3
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-08 07:32:29 +0530

    Changes in get Document file

--- getDocument.js
+++ getDocument.js
@@ -5,7 +5,15 @@ mongoose.connect("mongodb://localhost:27017/sbzchannel")
 .catch((err) => console.log(err));
 
 const playlistSchema = mongoose.Schema({
-
+    name: String,
+    ctype: String,
+    videos: Number,
+    author: String,
+    active: Boolean,
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 const Playlist = mongoose.model("Playlist", playlistSchema);
