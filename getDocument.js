9d7297ff05e09d3541f89706ff0fe1429fbc380d
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-08 07:23:06 +0530

    get Document learnt using find,select, limit & skip method

---
+++ getDocument.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/sbzchannel")
+.then(() => console.log("Connection established successfully..."))
+.catch((err) => console.log(err));
+
+const playlistSchema = mongoose.Schema({
+
+});
+
+const Playlist = mongoose.model("Playlist", playlistSchema);
+
+const getDocument = async () => {
+    try {
+        const result  = await Playlist.find();
+
+        // const result = await Playlist.find({ctype: "Back End"})
+
+        // const result = await Playlist.find({ctype: "Back End"}).select({name: 1});
+
+        // const result = await Playlist.find({ctype: "Front End"}).select({_id: 0, name: 1});
+
+        // const result = await Playlist.find({ctype: "Back End"}).select({_id: 0, name: 1}).limit(2);
+
+        // const result = await Playlist.find({ctype: "Back End"}).select({_id: 0, name: 1}).limit(2).skip(1);
+
+        console.log(result);
+    } 
+    catch(err) {console.log("Error: ",err)}
+}
+
+getDocument();
